527acf587a9b21403050d1a763b11663b3622b2a
Andreas Jakobsen <ahausgaardjakobsen@gmail.com>
2026-03-11 22:51:34 +0100

    bankruptcy unfinished.

--- src/business/services/StockBankruptService.java
+++ src/business/services/StockBankruptService.java
@@ -1,13 +1,36 @@
 package business.services;
 
+import domain.OwnedStock;
+import persistence.interfaces.OwnedStockDAO;
+import persistence.interfaces.PortfolioDAO;
+import persistence.interfaces.UnitOfWork;
 import shared.logging.Logger;
+import shared.logging.LoggerLevel;
+
+import java.util.List;
 
 public class StockBankruptService
 {
   private final Logger logger;
+  private final OwnedStockDAO ownedStockDAO;
+  private final PortfolioDAO portfolioDAO;
+  private final UnitOfWork uow;
 
-  public StockBankruptService(Logger logger)
+  public StockBankruptService(Logger logger, OwnedStockDAO ownedStockDAO,
+      PortfolioDAO portfolioDAO, UnitOfWork uow)
   {
     this.logger = logger;
+    this.ownedStockDAO = ownedStockDAO;
+    this.portfolioDAO = portfolioDAO;
+    this.uow = uow;
+  }
+
+  public void handleBankruptcy(String symbol)
+  {
+    logger.log(LoggerLevel.INFO, "Handling bankruptcy for stock: " + symbol);
+    uow.begin();
+
+
+
   }
 }
