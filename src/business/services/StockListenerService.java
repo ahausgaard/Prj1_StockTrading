b6f2b8085f1ba6cd0c4caa97415fef44c9f8c9c0
Andreas Jakobsen <ahausgaardjakobsen@gmail.com>
2026-03-11 22:26:10 +0100

    Add data directory configuration and refactor DAO implementations for consistency

--- src/business/services/StockListenerService.java
+++ src/business/services/StockListenerService.java
@@ -1,9 +1,14 @@
 package business.services;
 
+import business.dto.StockDTO;
 import business.dto.StockUpdateEvent;
 import business.stockmarket.StockMarketObserver;
 import domain.Stock;
 import domain.StockPriceHistory;
+import javafx.beans.property.ReadOnlyListProperty;
+import javafx.beans.property.ReadOnlyListWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import persistence.interfaces.StockDAO;
 import persistence.interfaces.StockPriceHistoryDAO;
 import persistence.interfaces.UnitOfWork;
@@ -19,12 +24,25 @@ public class StockListenerService implements StockMarketObserver
   private final StockDAO stockDAO;
   private final UnitOfWork uow;
 
+  private final ReadOnlyListWrapper<StockDTO> stocks =
+      new ReadOnlyListWrapper<>(FXCollections.observableArrayList());
+
   public StockListenerService(StockPriceHistoryDAO stockPriceHistoryDAO, StockDAO stockDAO, UnitOfWork uow)
   {
     this.logger = Logger.getInstance();
     this.stockPriceHistoryDAO = stockPriceHistoryDAO;
     this.stockDAO = stockDAO;
     this.uow = uow;
+  }
+
+  public ReadOnlyListProperty<StockDTO> stocksProperty()
+  {
+    return stocks.getReadOnlyProperty();
+  }
+
+  public ObservableList<StockDTO> getStocks()
+  {
+    return stocks.getReadOnlyProperty();
   }
 
   @Override
@@ -58,5 +76,8 @@ public class StockListenerService implements StockMarketObserver
     });
 
     uow.commit();
+
+
+    stocks.setAll(event.stocks());
   }
 }
