76f8b9777db833e84f7f0c26b9a0474d4dbfacd3
Andreas Jakobsen <ahausgaardjakobsen@gmail.com>
2026-03-12 11:39:29 +0100

    BankruptService finished

--- src/business/services/StockBankruptService.java
+++ src/business/services/StockBankruptService.java
@@ -2,35 +2,43 @@ package business.services;
 
 import domain.OwnedStock;
 import persistence.interfaces.OwnedStockDAO;
-import persistence.interfaces.PortfolioDAO;
 import persistence.interfaces.UnitOfWork;
 import shared.logging.Logger;
 import shared.logging.LoggerLevel;
-
-import java.util.List;
 
 public class StockBankruptService
 {
   private final Logger logger;
   private final OwnedStockDAO ownedStockDAO;
-  private final PortfolioDAO portfolioDAO;
   private final UnitOfWork uow;
 
-  public StockBankruptService(Logger logger, OwnedStockDAO ownedStockDAO,
-      PortfolioDAO portfolioDAO, UnitOfWork uow)
+  public StockBankruptService(Logger logger, OwnedStockDAO ownedStockDAO, UnitOfWork uow)
   {
     this.logger = logger;
     this.ownedStockDAO = ownedStockDAO;
-    this.portfolioDAO = portfolioDAO;
     this.uow = uow;
   }
 
   public void handleBankruptcy(String symbol)
   {
     logger.log(LoggerLevel.INFO, "Handling bankruptcy for stock: " + symbol);
+
     uow.begin();
 
+    OwnedStock ownedStock = ownedStockDAO.getBySymbol(symbol);
+    if (ownedStock != null)
+    {
+      ownedStockDAO.delete(symbol);
+      logger.log(LoggerLevel.INFO,
+          "Deleted OwnedStock for bankrupt symbol: " + symbol
+          + " (portfolioId: " + ownedStock.getPortfolioId() + ")");
+    }
+    else
+    {
+      logger.log(LoggerLevel.INFO,
+          "No OwnedStock found for bankrupt symbol: " + symbol + " — nothing to remove.");
+    }
 
-
+    uow.commit();
   }
 }
