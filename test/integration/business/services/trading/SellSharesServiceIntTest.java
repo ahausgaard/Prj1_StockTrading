3ce5dc4ffa82d0b3a0d0ef3d70bb07bcb9f2964c
Andreas Jakobsen <ahausgaardjakobsen@gmail.com>
2026-04-24 12:45:59 +0200

    All sell int tests

--- test/integration/business/services/trading/SellSharesServiceIntTest.java
+++ test/integration/business/services/trading/SellSharesServiceIntTest.java
@@ -16,8 +16,7 @@ import java.nio.file.Path;
 import java.util.Comparator;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SellSharesServiceIntTest
 {
@@ -84,4 +83,138 @@ public class SellSharesServiceIntTest
     assertEquals(1, transactions.size());
     assertEquals(TransactionType.SELL, transactions.getFirst().getType());
   }
+
+  @Test void sellShares_sellAllShares_deletesOwnedStockEntry() throws IOException
+  {
+    SellSharesRequest request = new SellSharesRequest(portfolio.getId(), "PNDORA", 10);
+    service.sellShares(request);
+
+    uow.begin();
+    List<OwnedStock> ownedStocks = ownedStockDAO.getAll();
+    Portfolio updatedPortfolio = portfolioDAO.getById(portfolio.getId());
+    List<Transaction> transactions = transactionDAO.getAll();
+    uow.commit();
+
+    // Quantity reduced from 10 to 5
+    assertEquals(0, ownedStocks.size());
+
+    // Balance credited
+    assertTrue(updatedPortfolio.getCurrentBalance().compareTo(new BigDecimal("10000.00")) > 0);
+
+    // Transaction recorded
+    assertEquals(1, transactions.size());
+    assertEquals(TransactionType.SELL, transactions.getFirst().getType());
+  }
+
+  @Test void sellShares_sellMoreThanOwned_fails() throws IOException
+  {
+    SellSharesRequest request = new SellSharesRequest(portfolio.getId(), "PNDORA", 12);
+    assertThrows(IllegalArgumentException.class, () -> service.sellShares(request));
+
+    uow.begin();
+    List<OwnedStock> ownedStocks = ownedStockDAO.getAll();
+    Portfolio updatedPortfolio = portfolioDAO.getById(portfolio.getId());
+    List<Transaction> transactions = transactionDAO.getAll();
+    uow.commit();
+
+    assertEquals(1, ownedStocks.size());
+    assertEquals(10, ownedStocks.getFirst().getQuantity());
+
+    assertEquals(0, updatedPortfolio.getCurrentBalance().compareTo(new BigDecimal("10000.00")));
+    assertEquals(0, transactions.size());
+  }
+
+  @Test void sellShares_stockNotFound_fails() throws IOException
+  {
+    SellSharesRequest request = new SellSharesRequest(portfolio.getId(), "AAPL", 1);
+    assertThrows(IllegalArgumentException.class, () -> service.sellShares(request));
+
+    uow.begin();
+    List<OwnedStock> ownedStocks = ownedStockDAO.getAll();
+    Portfolio updatedPortfolio = portfolioDAO.getById(portfolio.getId());
+    List<Transaction> transactions = transactionDAO.getAll();
+    uow.commit();
+
+    assertEquals(1, ownedStocks.size());
+    assertEquals(10, ownedStocks.getFirst().getQuantity());
+
+    assertEquals(0, updatedPortfolio.getCurrentBalance().compareTo(new BigDecimal("10000.00")));
+    assertEquals(0, transactions.size());
+  }
+
+  @Test void sellShares_invalidQuantity_fails() throws IOException
+  {
+    SellSharesRequest request = new SellSharesRequest(portfolio.getId(), "PNDORA", -1);
+    assertThrows(IllegalArgumentException.class, () -> service.sellShares(request));
+
+    uow.begin();
+    List<OwnedStock> ownedStocks = ownedStockDAO.getAll();
+    Portfolio updatedPortfolio = portfolioDAO.getById(portfolio.getId());
+    List<Transaction> transactions = transactionDAO.getAll();
+    uow.commit();
+
+    assertEquals(1, ownedStocks.size());
+    assertEquals(10, ownedStocks.getFirst().getQuantity());
+
+    assertEquals(0, updatedPortfolio.getCurrentBalance().compareTo(new BigDecimal("10000.00")));
+    assertEquals(0, transactions.size());
+  }
+
+  @Test void sellShares_stockNotOwned_fails() throws IOException
+  {
+    //Seed new stock
+    Stock unownedStock = Stock.createNew("AAPL", new BigDecimal("150.00"));
+    uow.begin();
+    stockDAO.create(unownedStock);
+    uow.commit();
+
+    SellSharesRequest request = new SellSharesRequest(portfolio.getId(), "AAPL", 1);
+    assertThrows(IllegalArgumentException.class, () -> service.sellShares(request));
+
+    uow.begin();
+    List<OwnedStock> ownedStocks = ownedStockDAO.getAll();
+    Portfolio updatedPortfolio = portfolioDAO.getById(portfolio.getId());
+    List<Transaction> transactions = transactionDAO.getAll();
+    uow.commit();
+
+    assertEquals(1, ownedStocks.size());
+    assertEquals(10, ownedStocks.getFirst().getQuantity());
+
+    assertEquals(0, updatedPortfolio.getCurrentBalance().compareTo(new BigDecimal("10000.00")));
+    assertEquals(0, transactions.size());
+  }
+
+  @Test void sellShares_bankrupStock_fails() throws IOException
+  {
+    //Seed and "buy" new stock
+    Stock bankruptStock = Stock.createFromStorage("BWE", StockState.BANKRUPT, new BigDecimal("50.00"));
+    OwnedStock bankruptOwnedStock = OwnedStock.createNew(portfolio.getId(), "BWE", 5);
+    uow.begin();
+    stockDAO.create(bankruptStock);
+    ownedStockDAO.create(bankruptOwnedStock);
+    uow.commit();
+
+    SellSharesRequest request = new SellSharesRequest(portfolio.getId(), "BWE", 1);
+    assertThrows(IllegalStateException.class, () -> service.sellShares(request));
+
+    uow.begin();
+    List<OwnedStock> ownedStocks = ownedStockDAO.getAll();
+    Portfolio updatedPortfolio = portfolioDAO.getById(portfolio.getId());
+    List<Transaction> transactions = transactionDAO.getAll();
+    uow.commit();
+
+    assertEquals(2, ownedStocks.size());
+    assertEquals(10, ownedStocks.getFirst().getQuantity());
+
+    assertEquals(0, updatedPortfolio.getCurrentBalance().compareTo(new BigDecimal("10000.00")));
+    assertEquals(0, transactions.size());
+  }
+
+
+
+
+
+
+
+
 }
